41242614f689a5a9b904bc8a89587b64c9eaaf81
Yaroslav <77915716+Yarik8706@users.noreply.github.com>
2026-01-12 22:05:34 +0300

    README

---
+++ Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace UI
+{
+    public class CloudMovement : MonoBehaviour
+    {
+        private Vector3 _startPosition;
+        [SerializeField] private Vector3 _endPosition;
+        
+        private void Start()
+        {
+            var _duration = Random.Range(20f, 25f);
+            _startPosition = transform.position;
+            _endPosition += transform.position;
+            DOTween.Sequence()
+                .Append(transform.DOMove(_endPosition, _duration))
+                .Append(transform.DOMove(_startPosition, _duration))
+                .SetEase(Ease.InOutSine)
+                .SetLoops(-1);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(_endPosition+transform.position, 0.1f);
+        }
+    }
+}
